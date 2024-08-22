5f70b30880ac83f1a5affae254f9ca8b61424cd2
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-08-22 06:02:38 -0700

    Feat: Add design and navabr in loginpage and interactions to api

--- src/views/auth/AuthLoginView.vue
+++ src/views/auth/AuthLoginView.vue
@@ -1,141 +1,164 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+//@ts-ignore
+import { useAxiosRequestWithToken } from '@/utils/service/axios_api'
+//@ts-ignore
+import { ApiRoutes } from '@/utils/service/endpoints/api'
+import { useRouter } from 'vue-router'
+//@ts-ignore
+import type { IUser, IUserAuth } from '@/utils/interface/user/IUser'
+//@ts-ignore
+import type { IToken } from '@/utils/interface/token'
+//@ts-ignore
+import { useToast } from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-sugar.css'
+//@ts-ignore
+import { setUser } from '@/stores/user'
+//@ts-ignore
+import { setToken } from '@/stores/token'
+//@ts-ignore
+import NavBarLogin from '@/components/navbar/NavBarLogin.vue'
+const router = useRouter()
+const toast = useToast()
+const loading = ref<Boolean>(false)
+const auth = ref<IUserAuth>({
+  UserName: '',
+  Password: ''
+})
+
+const login = async () => {
+  loading.value = true
+  const data = JSON.parse(JSON.stringify(auth.value))
+  const abortController = new AbortController()
+  const abortSignal = abortController.signal
+
+  const networkTimeout = setTimeout(() => {
+    abortController.abort()
+    loading.value = false
+    toast.open({
+      message: 'Network Error, please check your internet.',
+      type: 'error',
+      position: 'bottom',
+      duration: 5000
+    })
+  }, 30000)
+
+  await useAxiosRequestWithToken()
+    .post(`${ApiRoutes.login}`, data, { signal: abortSignal })
+    .then(function (response) {
+      //success
+      clearTimeout(networkTimeout)
+      const token = response.data.token
+      if (token != null) {
+        setToken(token as IToken)
+        setUser(response.data as IUser)
+      }
+      loading.value = false
+    })
+    .catch(function (error) {
+      //error
+      clearTimeout(networkTimeout)
+      toast.open({
+        message: error.response.data.message,
+        type: 'error',
+        position: 'bottom',
+        duration: 5000
+      })
+      loading.value = false
+    })
+}
+
+const loginValidate = async () => {
+  if (
+    !auth.value.Password ||
+    !auth.value.UserName ||
+    auth.value.Password.trim() === '' ||
+    auth.value.UserName.trim() === ''
+  ) {
+    toast.open({
+      message: 'Oops...Veuillez remplir vos informations!',
+      type: 'error',
+      position: 'bottom',
+      duration: 5000
+    })
+    return
+  } else {
+    await login()
+  }
+}
+</script>
+
 <template>
-  <div class="min-w-screen min-h-screen bg-green-600 flex items-center justify-center px-5 py-5">
-    <div class="w-full">
-      <div class="hidden md:block w-1/2 bg-gray-700 py-5 px-10">
+  <NavBarLogin />
+  <div
+    style="margin-top: -5%"
+    class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5"
+  >
+    <div class="w-full max-w-xl mx-auto bg-white rounded-md shadow-md overflow-hidden">
+      <div class="hidden md:block w-full bg-gray-700 py-5 px-10">
         <h1 class="text-white text-left text-2xl">Login</h1>
       </div>
-      <div class="w-full md:w-1/2 px-5 md:px-10 bg-white">
-        <div>
-          <form class="px-16" method="post">
-            <div class="flex flex-col -mx-3 pt-10">
-              <div class="w-full px-3 mb-3 flex items-center">
-                <label for="username" class="text-lg text-black px-1 w-1/4">User Name</label>
-                <input
-                  id="username"
-                  name="username"
-                  type="text"
-                  class="w-3/4 text-black pl-3 pr-3 py-1 border-2 border-gray-200 outline-none focus:border-gray-500"
-                />
-              </div>
-              <div class="w-full px-3 mb-4 flex items-center">
-                <label for="password" class="text-lg text-black px-1 w-1/4">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  class="w-3/4 text-black pl-3 pr-3 py-1 border-2 border-gray-200 outline-none focus:border-gray-500"
-                />
-              </div>
+      <div class="w-full">
+        <form class="px-8 py-10" @submit.prevent="loginValidate" method="post">
+          <div class="flex flex-col space-y-4">
+            <div class="flex items-center w-full">
+              <label for="username" class="text-lg text-black w-1/4">User Name</label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                v-model="auth.UserName"
+                class="w-3/4 text-black pr-3 pl-3 py-1 border-2 border-gray-200 outline-none focus:border-gray-500"
+              />
             </div>
-            <!-- <div class="flex items-center -mx-3 mb-8 px-3">
-              <input type="checkbox" id="remember" name="remember" class="mr-2" />
-              <label for="remember" class="text-black text-sm mr-auto">Remember Me</label>
-              <button
-                class="bg-gray-700 hover:bg-black focus:border-gray-500 rounded-md text-white text-lg px-3 py-1"
-                type="submit"
-              >
-                Login
-              </button>
-            </div> -->
-            <input type="checkbox" id="remember" name="remember" class="mr-2" />
-            <label for="remember" class="text-black text-sm mr-auto">Remember Me</label>
-            <div class="flex -mx-3 mb-8">
-              <div class="w-full px-3 mb-5">
-                <button
-                  class="max-w-xs mx-auto bg-gray-700 hover:bg-black focus:border-gray-500 rounded-md text-white text-lg px-3 py-1"
-                  type="submit"
-                >
-                  Login
-                </button>
-              </div>
-            </div>
-          </form>
-          <!-- <form v-else @submit.prevent="otpValidation" class="px-16" method="post">
-                <div class="flex -mx-3">
-                  <div class="w-full px-3 mb-3">
-                    <div class="mb-3">
-                      <label for="" class="text-lg text-black px-1">Identifiant</label>
-                    </div>
-                    <div class="flex">
-                      <div
-                        class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"
-                      ></div>
-                      <input
-                        id="otp"
-                        name="otp"
-                        type="text"
-
-                        class="w-full text-black -ml-10 pl-3 pr-3 py-1 border-2 border-gray-200 outline-none focus:border-gray-500"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div class="flex -mx-3 mb-8">
-                  <div class="w-full px-3 mb-5">
-                    <button
-
-                      class="max-w-xs mx-auto bg-gray-700 hover:bg-black focus:border-gray-500 rounded-md text-white text-lg px-3 py-1"
-                      type="submit"
-                    >
-                      <i class="fa fa-sign-in-alt"></i>
-                      Connexion
-                    </button>
-
-                  </div>
-                </div>
-              </form> -->
-        </div>
-      </div>
-      <!-- <div class="help">
-          <div class="bg-red-600 text-white mb-3 w-full max-w-xs p-4 rounded-lg shadow-lg">
-            <div class="flex justify-between items-center mb-4">
-              <h3 class="text-lg font-semibold"><i class="fa fa-question-circle mr-2"></i>Aide</h3>
-              <button class="text-white hover:text-gray-200">
-                <i class="fa fa-times"></i>
-              </button>
-            </div>
-            <div class="mb-4">
-              <p class="mb-2">Pour des mesures de securiter veuillez contacter ces numeros:</p>
-              <table class="w-full text-left text-sm">
-                <tbody>
-                  <tr class="border-b border-gray-200">
-                    <td class="py-1 px-2">Tel 1</td>
-                    <td class="py-1 px-2">0817300081</td>
-                  </tr>
-                  <tr class="border-b border-gray-200">
-                    <td class="py-1 px-2">Tel 2</td>
-                    <td class="py-1 px-2">0818303419</td>
-                  </tr>
-                  <tr class="border-b border-gray-200">
-                    <td class="py-1 px-2">Tel 3</td>
-                    <td class="py-1 px-2">0818303447</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="text-right">
-              <button class="bg-gray-500 text-white text-sm px-3 py-1 rounded hover:bg-gray-600">
-                Fermer
-              </button>
+            <div class="flex items-center w-full">
+              <label for="password" class="text-lg text-black w-1/4">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                v-model="auth.Password"
+                class="w-3/4 text-black pr-3 pl-3 py-1 border-2 border-gray-200 outline-none focus:border-gray-500"
+              />
             </div>
           </div>
-        </div> -->
+          <div class="flex flex-col items-start mt-4">
+            <div class="flex items-center w-full">
+              <label class="text-lg text-black w-1/4"></label>
+              <input type="checkbox" id="remember" name="remember" class="mr-2" />
+              <label for="remember" class="text-black text-sm">Remember Me</label>
+            </div>
+            <div class="flex items-center w-full">
+              <label class="text-lg text-black w-1/4"></label>
+              <button
+                class="bg-gray-700 hover:bg-black rounded-md text-white text-lg px-6 py-2 mt-4"
+                type="submit"
+                v-if="!loading"
+              >
+                <i class="fa fa-sign-in-alt"></i>
+                Login
+              </button>
+              <p v-else style="color: green">
+                <span class="fas fa-sync fa-spin"></span> Verification
+              </p>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
 <style>
-.help {
-  width: 300px;
-  height: 350px;
-  left: 50%;
-  top: 50%;
-  margin-left: -150px;
-  margin-top: -150px;
-  position: absolute;
-  z-index: 2;
-}
-.k-icon.k-i-reload {
-  -webkit-animation: spin 2s infinite linear;
+.fas.fa-sync {
   animation: spin 0.6s infinite linear;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
