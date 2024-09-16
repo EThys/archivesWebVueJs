6fa6029a85a562a4dc83f0757192817c302e541d
Muzola Ethberg <ethbergmuzola500@gmail.com>
2024-09-16 14:58:58 +0100

    Feat: Possibilites de faire une recherche et de voir les resulatats dans des tables

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -2,12 +2,31 @@
 import { ref } from 'vue'
 //@ts-ignore
 import NavBar from '@/components/navbar/NavBar.vue'
+//@ts-ignore
+import { useAxiosRequestWithToken } from '@/utils/service/axios_api'
+//@ts-ignore
+import { ApiRoutes } from '@/utils/service/endpoints/api'
+//@ts-ignore
+import { getToken } from '@/stores/token'
+//@ts-ignore
+import { getUser } from '@/stores/user'
+//@ts-ignore
+import type { IInvoice } from '@/utils/interface/invoice/IInvoice'
+const searchTerm = ref('')
+import axios from 'axios'
 
+const token = getToken() as string
 const isCategorySearchOpen = ref(false)
+const user = getUser()
 const isChecked = ref<boolean>(false)
 const selectedItem = ref('All')
 const showSortOptions = ref<boolean>(false)
 const advancedSearch = ref<boolean>(false)
+const invoices = ref([])
+const currentPage = ref(1)
+const totalPages = ref(0)
+const perPage = ref(20)
+
 const toggleSortOptions = () => {
   showSortOptions.value = !showSortOptions.value
 }
@@ -25,6 +44,67 @@ const items = ref([
   'BranchName',
   'Date'
 ])
+const getCurrentDate = () => {
+  var date = new Date()
+  var year = date.getFullYear()
+  var month = String(date.getMonth() + 1).padStart(2, '0')
+  var day = String(date.getDate()).padStart(2, '0')
+  return `${year}-${month}-${day}`
+}
+const invoice = ref<IInvoice>({
+  ClientPhone: '',
+  InvoiceCode: '',
+  InvoiceDesc: '',
+  InvoiceBarCode: '',
+  DirectoryFId: 0,
+  subFolder: 0,
+  InvoiceKeyFId: 0,
+  dataCreated: '',
+  InvoicePath: '',
+  ClientName: '',
+  ExpiredDate: getCurrentDate(),
+  AndroidVersion: '',
+  UserFId: user!.UserId,
+  InvoiceDate: getCurrentDate(),
+  BranchFId: user!.BranchFId,
+  dateFrom: '',
+  dateTo: '',
+  images: [],
+  isActive: false
+})
+const loading = ref(false)
+const pagination = ref()
+
+const searchInvoices = async () => {
+  loading.value = true
+  const searchParams = new URLSearchParams()
+  let key
+  switch (selectedItem.value) {
+    case 'All':
+      key = 'All'
+      break
+    case 'Description':
+      key = 'InvoiceDesc'
+      break
+    default:
+      key = `Invoice${selectedItem.value}`
+  }
+
+  searchParams.append(key, searchTerm.value)
+  searchParams.append('per_page', perPage.value.toString())
+
+  try {
+    const response = await useAxiosRequestWithToken(token).get(
+      `${ApiRoutes.searchInvoice}?${searchParams.toString()}`
+    )
+    invoices.value = response.data.data.data
+    pagination.value = response.data.data // Pagination data
+    console.log('eeeeeeeebababa', pagination.value)
+  } catch (error) {
+    console.log(error)
+  }
+  loading.value = false
+}
 
 const selectItem = (item: string) => {
   selectedItem.value = item
@@ -99,12 +179,14 @@ const toggleCategorySearchOpen = () => {
         <div class="relative w-full">
           <input
             type="search"
+            v-model="searchTerm"
             id="search-dropdown"
             class="block py-2 px-4 w-full z-20 text-lg text-gray-900 bg-white rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
             placeholder="What are you looking for?"
             required
           />
           <button
+            @click.prevent="searchInvoices()"
             type="submit"
             class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-gray-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
@@ -182,6 +264,9 @@ const toggleCategorySearchOpen = () => {
       </button>
     </div>
   </div>
+  <div v-if="loading" class="loader flex justify-center items-center h-32">
+    <i style="font-size: 50px" class="fa-solid fa-spinner fa-spin"></i>
+  </div>
   <div
     style="width: 69%"
     class="ethberge bg-gray-700 rounded-md pt-2 pb-5 px-4 mx-auto mt-3"
@@ -190,19 +275,23 @@ const toggleCategorySearchOpen = () => {
     <div class="flex items-center">
       <div class="flex-1 mr-4">
         <label for="sort-by" class="text-md font-semibold text-gray-400">Sort By</label>
-        <input
-          type="text"
+        <select
           id="sort-by"
-          class="w-full px-3 py-2 border border-gray-700 rounded-md"
-        />
+          class="mt-1 text-black-400 p-2 w-full border border-gray-700 rounded-md"
+        >
+          <option>Branch</option>
+          <option>Time</option>
+        </select>
       </div>
       <div class="flex-1 mr-4">
         <label for="order-by" class="text-md font-semibold text-gray-400">Order By</label>
-        <input
-          type="text"
+        <select
           id="order-by"
-          class="w-full px-3 py-2 border border-gray-700 rounded-md"
-        />
+          class="mt-1 text-black-400 p-2 w-full border border-gray-700 rounded-md"
+        >
+          <option>Desc</option>
+          <option>Asc</option>
+        </select>
       </div>
       <div class="flex-2 mr-4">
         <label for="order-by" class="text-md block font-semibold text-gray-400">Actions</label>
@@ -217,6 +306,7 @@ const toggleCategorySearchOpen = () => {
           Clear
         </button>
         <button
+          @click="showSortOptions = false"
           class="bg-blue-400 hover:bg-blue-500 hover:text-white text-black font-medium text-sm py-2 px-4 rounded"
         >
           <i class="fa-solid fa-xmark"></i>
@@ -352,6 +442,7 @@ const toggleCategorySearchOpen = () => {
             Clear
           </button>
           <button
+            @click="advancedSearch = false"
             class="bg-blue-400 hover:bg-blue-500 hover:text-white text-black font-medium text-sm py-2 px-4 rounded"
           >
             <i class="fa-solid fa-xmark"></i>
@@ -361,4 +452,220 @@ const toggleCategorySearchOpen = () => {
       </div>
     </div>
   </div>
+  <div
+    style="width: 69%"
+    v-if="invoices.length < 0 && !loading"
+    class="block justify-between mx-auto mt-8 text-center text-white bg-red-400 py-4 rounded-sm"
+  >
+    No results found
+  </div>
+  <div v-if="invoices.length > 0" style="width: 69%" class="block justify-between mx-auto mt-3">
+    <div class="flex justify-between items-center mb-4 mt-8">
+      <a class="ml-1 text-blue-500 text-md underline cursor-pointer"> Select All |</a>
+      <a style="margin-left: -20%" class="ml-1 text-blue-500 text-md underline cursor-pointer">
+        Deselect All
+      </a>
+      <div class="flex space-x-2">
+        <button class="bg-gray-300 text-blue-400 py-2 px-4 rounded">Prev</button>
+        <button class="bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded">
+          1
+        </button>
+        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+          2
+        </button>
+        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+          3
+        </button>
+        <span class="text-gray-500">...</span>
+        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+          15
+        </button>
+        <button class="bg-gray-300 text-gray-800 py-2 px-4 rounded">Next</button>
+      </div>
+      <span class="text-gray-500" v-if="invoices.length > 20">
+        1 to 20 of {{ invoices.length }}
+      </span>
+      <span class="text-gray-500" v-else>
+        1 to {{ invoices.length }} of {{ invoices.length }}
+      </span>
+    </div>
+
+    <div
+      v-for="invoice in invoices"
+      :key="invoice"
+      class="relative overflow-x-auto shadow-md sm:rounded-sm border mb-3 border-gray-200"
+    >
+      <table
+        class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 custom-table"
+      >
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              Id
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">
+              {{ invoice.invoiceId }}
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              Key
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">
+              {{ invoice.InvoiceKeyFId }}
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              Code
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">
+              {{ invoice.InvoiceCode }}
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              Description
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">
+              {{ invoice.InvoiceDesc }}
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              BarCode
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">
+              {{ invoice.InvoiceBarCode }}
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              Username
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">User123</td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              Directory
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">
+              {{ invoice.DirectoryFId }}
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              Branch
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">
+              {{ invoice.BranchFId }}
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              Date
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">
+              {{ invoice.InvoiceDate }}
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-gray-300 dark:border-gray-600 w-1/6"
+            >
+              Files
+            </th>
+            <td class="px-6 py-2 border-r border-gray-300 dark:border-gray-600 w-1/4">file1.txt</td>
+            <td class="px-6 py-4 border-r border-gray-300 dark:border-gray-600 w-1/4">
+              <div class="flex justify-end space-x-2" aria-rowspan="10">
+                <button
+                  class="edit bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Edit
+                </button>
+                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                  Delete
+                </button>
+                <button
+                  class="save bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Save
+                </button>
+                <button
+                  class="select bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Select
+                </button>
+                <button class="view bg-black text-white font-bold py-2 px-4 rounded">View</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
+<style>
+.custom-table td:not(:last-child),
+.custom-table th:not(:last-child) {
+  border-bottom: 1px solid #e5e7eb;
+}
+.custom-table th,
+.custom-table td {
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+.custom-table td:nth-child(2) {
+  text-align: right;
+  color: black;
+}
+
+.custom-table td:last-child,
+.custom-table th:last-child {
+  border-bottom: none;
+}
+
+.action-buttons .edit {
+  color: gray;
+}
+
+.action-buttons .save {
+  color: green;
+}
+
+.action-buttons .select {
+  color: blue;
+}
+
+.action-buttons .view {
+  color: black;
+}
+</style>
